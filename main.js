810137d26f3587a53b2b121a3ed9f96e2390343c
Lucas de Souza Pegoretti <lucasouzape@gmail.com>
2026-06-18 15:33:04 -0300

    teste corrigido

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     
+    // Seleciona a opção "Sim" pelo ID
+    const opcaoSim = document.getElementById('isim');
     
-    const opcaoComCerteza = document.getElementById('icomctz');
-    
-   
+    // Seleciona todos os inputs que estão dentro das divs com a classe "armadilha"
     const opcoesNegativas = document.querySelectorAll('.armadilha input');
 
-    
+    // Para cada opção negativa, adiciona um evento que escuta o clique
     opcoesNegativas.forEach(function(checkboxNegativo) {
         checkboxNegativo.addEventListener('click', function(event) {
             
-           
+            // Impede que a caixinha negativa seja marcada
             event.preventDefault(); 
             
-            
-            opcaoComCerteza.checked = true; 
+            // Marca a opção "Sim" instantaneamente
+            opcaoSim.checked = true; 
             
         });
     });
-}); //test
+});
