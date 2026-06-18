0c206912bc2e362a273d39fe18dbf0fa6f911e08
Lucas de Souza Pegoretti <lucasouzape@gmail.com>
2026-06-18 15:40:19 -0300

    ajuste php

--- cadastro.php
+++ cadastro.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resposta Enviada!</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="form-card">
-        <h1>Eu sabia! 🥰</h1>
-        <p style="font-size: 1.5rem; color: #e84393; font-weight: bold; margin-top: 20px;">
+
+Eu sabia! 🥰
+       
             Eu te amo ❤️
-        </p>
-    </div>
-</body>
-</html>
